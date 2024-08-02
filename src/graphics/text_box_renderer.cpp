f44309eb34c7c0b7133743ec1e427256b6f56203
Michal Havlíček <michalhav7@seznam.cz>
2024-08-02 21:35:40 +0200

    use widget visitor in textbox renderer

--- src/graphics/text_box_renderer.cpp
+++ src/graphics/text_box_renderer.cpp
@@ -3,6 +3,7 @@
 #include "text_box.hpp"
 #include "raylib/raylib.h"
 #include "ui/text.hpp"
+#include "ui/widget_visitor.hpp"
 
 Text_Box_Renderer::Text_Box_Renderer(const Text_Box& text_box) :
 	m_text_box{text_box}
@@ -29,11 +30,16 @@ void Text_Box_Renderer::draw(float time_delta)
 
 	if (m_text_box.contains_message()) {
 		assert(m_widget);
-		// FIXME - do this cast safely
-		UI::Text* text_widget = (UI::Text*)m_widget->get_widget_by_name("Text");
-		assert(text_widget);
 
-		text_widget->text = m_text_box.get_displayed_message().substring(0, m_text_box.get_displayed_character_count());
+		UI::Widget* widget = m_widget->get_widget_by_name("Text");
+
+		auto visitor = UI::Widget_Visitor::create_text_visitor([&](UI::Text& text_widget){
+			text_widget.text = m_text_box.get_displayed_message().substring(0, m_text_box.get_displayed_character_count());
+		});
+
+		widget->accept_visitor(visitor);
+
+		assert(widget);
 	}
 
 	m_widget->draw_as_root(time_delta);
--- src/graphics/ui/widget_visitor.hpp
+++ src/graphics/ui/widget_visitor.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "utils/concepts.hpp"
+
 namespace UI
 {
 
@@ -7,6 +9,7 @@ class Box;
 class Button;
 class Image;
 class Text;
+class Widget;
 
 class Widget_Visitor
 {
@@ -15,6 +18,41 @@ public:
 	virtual void visit_button(Button& text) = 0;
 	virtual void visit_image(Image& image) = 0;
 	virtual void visit_text(Text& text) = 0;
+
+	template<typename Fn>
+	requires Concepts::Callable<Fn, Text&>
+	static auto create_text_visitor(Fn fn);
 };
+
+
+// Implemantation
+
+template<typename Fn>
+requires Concepts::Callable<Fn, Text&>
+auto Widget_Visitor::create_text_visitor(Fn fn)
+{
+	class Derived_Visitor : public Widget_Visitor
+	{
+	public:
+		Derived_Visitor(Fn fn_) :
+			m_fn{fn_}
+		{
+		}
+
+	private:
+
+		virtual void visit_box(Box&) override {}
+		virtual void visit_button(Button&) override {}
+		virtual void visit_image(Image&) override {}
+		virtual void visit_text(Text& text) override
+		{
+			m_fn(text);
+		}
+
+		Fn m_fn; // Store the lambda here to prevent dangling pointer
+	};
+
+	return Derived_Visitor{fn};
+}
 
 }

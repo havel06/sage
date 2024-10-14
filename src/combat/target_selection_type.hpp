a4149f0173498c7a970a35d6c4f9f7d4685af7a9
Michal Havlíček <michalhav7@seznam.cz>
2024-10-14 20:12:02 +0200

    remove Target_Selection_Type::self

--- src/combat/target_selection_type.hpp
+++ src/combat/target_selection_type.hpp
@@ -7,7 +7,6 @@ enum class Target_Selection_Type
 {
 	enemy, // Select any enemy
 	ally,  // Select any ally
-	self,  // Automatically selects the character casting the ability FIXME - consider removing
 };
 
 inline Target_Selection_Type target_selection_type_from_str(const String& str)
@@ -16,8 +15,6 @@ inline Target_Selection_Type target_selection_type_from_str(const String& str)
 		return Target_Selection_Type::enemy;
 	if (str == "ally")
 		return Target_Selection_Type::ally;
-	if (str == "self")
-		return Target_Selection_Type::self;
 
 	assert(false);
 }

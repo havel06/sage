fe4bac983771c88472403c667e6347ef31955dfa
Michal Havlíček <michalhav7@seznam.cz>
2024-12-20 15:31:31 +0100

    imgui column padding

--- src/graphics/imgui/widgets/column.cpp
+++ src/graphics/imgui/widgets/column.cpp
@@ -1,6 +1,7 @@
 #include "column.hpp"
 #include "utils/minmax.hpp"
 #include "utils/move.hpp"
+#include "../theme.hpp"
 
 namespace IMGUI::Widgets
 {
@@ -18,6 +19,7 @@ void Column::draw()
 
 Vec2i Column::layout(Recti bounding_box)
 {
+	const int padding = Theme::PADDING_DEFAULT;
 	int height = 0;
 	int width = 0;
 
@@ -37,7 +39,7 @@ Vec2i Column::layout(Recti bounding_box)
 		const Vec2i child_size = child->layout(child_transfom);
 
 		width = max(width, child_size.x); // Width is maximum child width
-		height += child_size.y; // Height is the sum of children heights
+		height += child_size.y + padding; // Height is the sum of children heights
 	}
 
 	return Vec2i{width, height};

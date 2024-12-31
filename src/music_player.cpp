b06328b9416633349a45b537ea04f3de823dc466
Michal Havlíček <michalhav7@seznam.cz>
2024-12-31 14:15:37 +0100

    bring back music player

--- src/music_player.cpp
+++ src/music_player.cpp
@@ -3,14 +3,18 @@
 
 void Music_Player::play(Resource_Handle<Sound> new_music)
 {
-	//StopSound(m_current_music);
-	//PlaySound(new_music);
+	if (m_current_music.has_value())
+		StopSound(m_current_music.value().get());
 
+	PlaySound(new_music.get());
 	m_current_music = new_music;
 }
 
 void Music_Player::update()
 {
-	//if (!IsSoundPlaying(m_current_music))
-		//PlaySound(m_current_music);
+	if (!m_current_music.has_value())
+		return;
+
+	if (!IsSoundPlaying(m_current_music.value().get()))
+		PlaySound(m_current_music.value().get());
 }
